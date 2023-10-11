317b0450b9c5326832dd5a672349d7cfd61c5ed9
mathispappo <mathispappo@gmail.com>
2023-10-11 14:25:28 +0200

    debut react

---
+++ main/src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from "./assets/inEYES.png";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        -- Data Camp Eyes Cancer --
+        <img src={logo} className="App-logo" alt="logo" />
+
+        
+      </header>
+      <body>
+        <p>mvsbevmobmssrns</p>
+      </body>
+    </div>
+  );
+}
+
+export default App;
