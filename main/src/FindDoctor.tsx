c42cfb34c4c45a70055d231fc1ccecb5636e9391
GabrielAmanou <gabriel.amanou@efrei.net>
2023-10-26 20:51:52 +0200

    sauvegarde

--- main/src/FindDoctor.tsx
+++ main/src/FindDoctor.tsx
@@ -2,9 +2,10 @@ import React, { useEffect, useRef, useState } from 'react';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
 import './App.css';
-import Mathis from "./assets/MathisPappo.jpg";
-import Loupe from "./assets/loupe.png";
-import AidKit from "./assets/first-aid-kit.png";
+import Female from "./assets/Profile F.png";
+import Male from "./assets/Profile M.png";
+import icon from "./assets/Loc.png";
+import doctors from './doctors'; // Importez la liste de médecins depuis le fichier doctors.js
 
 
 const FindDoctor: React.FC = () => {
@@ -32,6 +33,17 @@ const FindDoctor: React.FC = () => {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
+      doctors.forEach((doctor) => {
+        const customIcon = L.icon({
+          iconUrl: icon, // Spécifiez l'URL de l'icône personnalisée
+          iconSize: [30, 30], // Spécifiez la taille de l'icône (ajustez-la en fonction de votre image)
+          iconAnchor: [15, 30], // Spécifiez l'ancre de l'icône (ajustez-la en fonction de votre image)
+        });
+      
+        L.marker([doctor.latitude, doctor.longitude], { icon: customIcon }).addTo(map);
+      
+      });
+
       // Stockons la référence de la carte dans la variable de ref
       mapRef.current = map;
     }
@@ -40,31 +52,26 @@ const FindDoctor: React.FC = () => {
   return (
     <div className='formMap'>
       <div className='formulaire'>
-        <form onSubmit={handleSearchSubmit} style={{paddingBottom: "30px" }}>
-          <input
-            type="text"
-            placeholder="chercher un docteur"
-            value={searchValue}
-            onChange={handleSearchChange}
-            style={{ textAlign: 'center', borderRadius: '50px' }}
-          />
-          <button type="submit">
-            <img src={Loupe} alt="loupe" style={{ height: "20px", width: "20px", marginLeft: "10px" }}/>
-          </button>
+        <form onSubmit={handleSearchSubmit} style={{ paddingBottom: "30px" }}>
+          {/* ... */}
         </form>
-        <div className="profilDoc">
-          <img className="w-20 h-20 rounded-full m-2" src={Mathis} alt=""/>
-          <div className="font-medium dark:text-white m-3">
-            <div>Dr. Tanpa Pamrih</div>
-            <div className="text-sm text-gray-500 dark:text-gray-400">General Practitioner</div>
+        <div className="doctor-list-container">
+        {doctors.map((doctor, index) => (
+          <div className="doctor-profile" key={index}>
+            {/* Image en fonction du sexe du médecin */}
+            {doctor.gender === 'Male' ? (
+              <img src={Male} alt="Male" className="doctor-image" />
+            ) : (
+              <img src={Female} alt="Female" className="doctor-image" />
+            )}
+
+            <div className="doctor-name">{doctor.firstName} {doctor.lastName}</div>
+            <div className="doctor-specialization">{doctor.specialization}</div>
           </div>
-        </div>
-        <div className='experience'>
-          <img src={AidKit} alt="Aid-Kit" style={{ height: "20px", width: "20px" }}/>
-          <div>5 year experiences</div>
-        </div>
+        ))}
       </div>
-      <div id="map" className='maps' style={{ height: 'calc(100vh - 120px)', width: '1000px' }}/>
+      </div>
+      <div id="map" className='maps' style={{ height: 'calc(100vh - 120px)', width: '1000px' }} />
     </div>
   );
 };
